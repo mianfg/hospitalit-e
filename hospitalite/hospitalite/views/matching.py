5d16762d5009f8a5db8a0e929c9e9e7c09bd85d7
mianfg <mianfg19@gmail.com>
2020-04-06 04:28:20 +0200

    Renderizar plantilla

--- hospitalite/hospitalite/views/matching.py
+++ hospitalite/hospitalite/views/matching.py
@@ -4,6 +4,25 @@ from hospitalite.models import Usuario
 
 @login_required
 def index(response):
-    user_id = Usuario.objects.get(username=response.user.username)
+    user = Usuario.objects.get(username=response.user.username)
+    is_voluntario = user.is_voluntario()
+    first_name = user.first_name
 
-    return render(response, "matching.html", {'user_id': user_id})
+    text = "Si quieres entrar en contacto con alguien, sólo tienes que pulsar en el siguiente botón, y buscaremos voluntarios disponibles"
+    button_text = "Buscar voluntarios"
+    search_text = "Estamos buscando voluntarios, espere un momento"
+
+    if is_voluntario:
+        text = "Pulsa en el siguiente botón para contactar con un enfermo"
+        search_text = "Estamos buscando enfermos, espere un momento"
+        button_text = "Contactar con un enfermo"
+
+    items = {
+        'is_voluntario' : is_voluntario,
+        'first_name'    : first_name,
+        'text'          : text,
+        'button_text'   : button_text,
+        'search_text'   : search_text
+    }
+
+    return render(response, "matching.html", items)
